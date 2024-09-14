d69feae2cb590e8c0958433ba36875843fad2123
Jonathan He <d3rk12345678@gmail.com>
2024-09-14 13:56:13 -0400

    implemenation of profile editing, profile and image upload

--- EventsSocialMedia/services/userService.js
+++ EventsSocialMedia/services/userService.js
@@ -1,5 +1,4 @@
 import { supabase } from "../lib/supabase";
-
 export const getUserData = async (userId) => {
   try {
     const { data, error } = await supabase
@@ -7,13 +6,29 @@ export const getUserData = async (userId) => {
       .select()
       .eq("id", userId)
       .single();
+    if (error) {
+      return { success: false, msg: error?.message };
+    }
+    return { success: true, data };
+  } catch (error) {
+    console.log(`got error : `, error);
+    return { success: false, msg: error.message };
+  }
+};
+
+export const updateUser = async (userId, data) => {
+  try {
+    const { error } = await supabase
+      .from("users")
+      .update(data)
+      .eq("id", userId);
 
     if (error) {
       return { success: false, msg: error?.message };
     }
     return { success: true, data };
   } catch (error) {
-    console.log("got error: ", error);
+    console.log(`got error : `, error);
     return { success: false, msg: error.message };
   }
 };
