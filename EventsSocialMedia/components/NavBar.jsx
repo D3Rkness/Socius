54723ab7b830133ffa1efe3bf7d0546c023b13d4
Your Name <you@example.com>
2024-09-15 00:57:24 -0400

    improve navbar

--- EventsSocialMedia/components/NavBar.jsx
+++ EventsSocialMedia/components/NavBar.jsx
@@ -1,37 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, TouchableOpacity, StyleSheet } from "react-native";
 import Icon from "../assets/icons";
-import { useRouter } from "expo-router";
+import { useRouter, usePathname } from "expo-router";
+import { theme } from "../constants/theme";
 
 const NavBar = () => {
   const size = 22;
   const router = useRouter();
+  const currentRoute = usePathname().split("/")[1];
+
+  const [iconColors, setIconColors] = useState({
+    home: theme.colors.textDark,
+    search: theme.colors.textDark,
+    events: theme.colors.textDark,
+    chat: theme.colors.textDark,
+    profile: theme.colors.textDark,
+  });
+
+  useEffect(() => {
+    setIconColors({
+      home:
+        currentRoute === "home" ? theme.colors.primary : theme.colors.textDark,
+      search:
+        currentRoute === "search"
+          ? theme.colors.primary
+          : theme.colors.textDark,
+      events:
+        currentRoute === "events"
+          ? theme.colors.primary
+          : theme.colors.textDark,
+      chat:
+        currentRoute === "chat" ? theme.colors.primary : theme.colors.textDark,
+      profile:
+        currentRoute === "profile"
+          ? theme.colors.primary
+          : theme.colors.textDark,
+    });
+  }, [currentRoute]);
+  const navigate = (route) => {
+    if (currentRoute === route) return;
+    router.push(route);
+  };
 
   return (
     <View style={styles.navbar}>
-      <TouchableOpacity
-        style={styles.navItem}
-        onPress={() => router.push("home")}
-      >
-        <Icon name="home" size={size + 1} />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.navItem}>
-        <Icon name="search" size={size} />
+      <TouchableOpacity style={styles.navItem} onPress={() => navigate("home")}>
+        <Icon name="home" size={size + 1} color={iconColors.home} />
       </TouchableOpacity>
       <TouchableOpacity
         style={styles.navItem}
-        onPress={() => router.push("events")}
+        onPress={() => navigate("search")}
       >
-        <Icon name="events" size={size + 1} />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.navItem}>
-        <Icon name="chat" size={size} />
+        <Icon name="search" size={size} color={iconColors.search} />
       </TouchableOpacity>
       <TouchableOpacity
         style={styles.navItem}
-        onPress={() => router.push("profile")}
+        onPress={() => navigate("events")}
       >
-        <Icon name="user" size={size} />
+        <Icon name="events" size={size + 1} color={iconColors.events} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.navItem} onPress={() => navigate("chat")}>
+        <Icon name="chat" size={size} color={iconColors.chat} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.navItem}
+        onPress={() => navigate("profile")}
+      >
+        <Icon name="user" size={size} color={iconColors.profile} />
       </TouchableOpacity>
     </View>
   );
