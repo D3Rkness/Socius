e6196a4576a7449415f246bbd9401d98695b9790
Jonathan He <d3rk12345678@gmail.com>
2024-09-14 21:28:23 -0400

    rename app Socius

---
+++ Socius/app/_layout.jsx
@@ -0,0 +1,48 @@
+import { View, Text, LogBox } from "react-native";
+import React, { useEffect } from "react";
+import { Stack, useRouter } from "expo-router";
+import { AuthProvider, useAuth } from "../contexts/AuthContext";
+import { supabase } from "../lib/supabase";
+import { getUserData } from "../services/userService";
+
+LogBox.ignoreLogs([
+  "Warning: TRenderEngineProvider",
+  "Warning: MemoizedTNodeRenderer",
+  "Warning: TNodeChildrenRenderer",
+]);
+const _layout = () => {
+  return (
+    <AuthProvider>
+      <MainLayout />
+    </AuthProvider>
+  );
+};
+
+const MainLayout = () => {
+  const { setAuth, setUserData } = useAuth();
+  const router = useRouter();
+  const updateUserData = async (user, email) => {
+    let res = await getUserData(user?.id);
+    if (res.success) {
+      setUserData({ ...res.data, email });
+    }
+  };
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_event, session) => {
+      console.log("session user : ", session?.user?.id);
+
+      if (session) {
+        setAuth(session?.user);
+        updateUserData(session?.user, session?.user?.email);
+        router.replace("/home");
+      } else {
+        setAuth(null);
+        router.replace("/welcome");
+      }
+    });
+  }, []);
+
+  return <Stack screenOptions={{ headerShown: false }} />;
+};
+
+export default _layout;
